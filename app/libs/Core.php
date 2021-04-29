ef4f5924cda47bf4edb8deafae75e9452fff3346
peeretus purgis <shitfacedidcoke@gmail.com>
2021-04-29 22:10:49 +0300

    Kontroller määratud ning andmed on valmis kasutuseks

--- app/libs/Core.php
+++ app/libs/Core.php
@@ -1,20 +1,33 @@
 <?php
 
-
 class Core
-{ // Class begins
-    /**
-     * Core constructor.
-     */
-    public function __construct()
-    {
-        $this->getUrl();
+{
+    protected $currentController = 'Pages';
+    protected $currentMethod = 'index';
+    protected $params = [];
+    // constructor
+    public function __construct () {
+        $url = $this->getUrl();
+        $controllerName = ucwords($url[0]);
+        $controllerFile = '../app/controllers/'.$controllerName.'.php';
+        if(file_exists($controllerFile)) {
+            $this->currentController = $controllerName;
+            unset($url[0]);
+        }
+        require_once '../app/controllers/'.$this->currentController.'.php';
+        $this->currentController = new $this->currentController;
+        print_r($this->currentController);
+        print_r($url);
     }
     // get url data
-    public function getUrl(){
-        if(isset($_GET['url'])){
+    public function getUrl() {
+        if (isset($_GET['url'])) {
             $url = $_GET['url'];
-            echo $url;
+            $url = rtrim($url, '/');
+            $url = htmlentities($url);
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url  = explode('/', $url );
+            return $url;
         }
     }
-} // Class ends
+}
