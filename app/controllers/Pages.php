1d0e4c40b82e3b92fa68f1f70530cbe794dc2e32
peeretus purgis <shitfacedidcoke@gmail.com>
2021-04-30 21:01:58 +0300

    Põhikontroller loomine, vaade faili laadimine

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -5,6 +5,14 @@ class Pages
 {
     public function __construct()
     {
-        echo "pages class is loaded <br>";
+        echo 'Pages Class is loaded<br>';
+    }
+
+    public function index(){
+        echo 'index meth is loaded<br>';
+    }
+
+    public function about(){
+        echo 'about meth is loaded<br>';
     }
 }
