48353af130d09c488dd959cd7204a8eeb5fdefd0
peeretus purgis <shitfacedidcoke@gmail.com>
2021-05-01 20:54:40 +0300

    konstandid

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,4 +1,5 @@
 <?php
-//load libraries
+// load libraries
+require_once 'config/constants.php';
 require_once 'libs/Core.php';
 require_once  'libs/Controller.php';
---
+++ app/config/constants.php
@@ -0,0 +1,7 @@
+<?php
+// Project name
+define('PROJECT_NAME', 'raamistik');
+// Aop root dir
+define('APPROOT',dirname(dirname(__FILE__)));
+// URL root
+define('URLROOT', 'http://'.$_SERVER['HTTP_HOST'].'/'.'raamistik');
