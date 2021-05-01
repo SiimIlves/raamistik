27348417946525b95189a66f1647991f5f74b274
peeretus purgis <shitfacedidcoke@gmail.com>
2021-05-01 13:56:24 +0300

    vaade täiendamine

--- app/libs/Core.php
+++ app/libs/Core.php
@@ -1,40 +1,40 @@
 <?php
 
-
 class Core
 { // Class begins
     protected $currentController = 'Pages';
-    protected  $currentMethod = 'index';
+    protected $currentMethod = 'index';
     protected $params = [];
     public function __construct()
     {
         $url = $this->getUrl();
         $controllerName = ucwords($url[0]);
         $controllerFile = '../app/controllers/'.$controllerName.'.php';
-        if(file_exists($controllerFile)){
+        if(file_exists($controllerFile)) {
             $this->currentController = $controllerName;
             unset($url[0]);
         }
-        require_once  '../app/controllers/' .$this->currentController.'.php';
+        require_once  '../app/controllers/'.$this->currentController.'.php';
         $this->currentController = new $this->currentController;
 
-        if(method_exists($this->currentController, $url[1])){
+        if(method_exists($this->currentController, $url[1])) {
             $this->currentMethod = $url[1];
             unset($url[1]);
         }
-       $this->params = $url ? array_values($url) : array();
+       $this->params = $url ? array_values($url) : [];
 
         call_user_func_array(array($this->currentController, $this->currentMethod), $this->params);
 
     }
     // get url data
-    public function  getUrl() {
-        if(isset($_GET['url'])){
+    public function getUrl() {
+        if (isset($_GET['url']))
+        {
             $url = $_GET['url'];
             $url = rtrim($url, '/');
             $url = htmlentities($url);
             $url = filter_var($url, FILTER_SANITIZE_URL);
-            $url = explode('/', $url);
+            $url  = explode('/', $url );
             return $url;
         }
     }
