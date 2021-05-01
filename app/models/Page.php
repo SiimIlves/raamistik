2ea95babb7c6f945f40ac255c0dc9ab8c56cdeb8
peeretus purgis <shitfacedidcoke@gmail.com>
2021-05-01 21:51:59 +0300

    Andmete lugemine mitme rea kaupa

--- app/models/Page.php
+++ app/models/Page.php
@@ -23,4 +23,9 @@ class Page
         $this->db->bind('id', 1);
         return $this->db->getOne();
     }
+
+    public function getUsers() {
+        $this->db->query('SELECT * FROM users');
+        return $this->db->getAll();
+    }
 }
